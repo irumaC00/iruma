386c99812ec95e210916e7fd8bfabff137b74966
Iruma Shibuya <programiruma@gmail.com>
2012-08-25 01:25:56 -0400

    Create new directory playWithRails, create test_app

---
+++ playWithRails/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '6299ad29d334194f3b1b83f7b45f837b395f37d5464283c3e2cff3cdb16ad04c4301231eb39e3f7ce33a3e0ec96b111abd408199bdff283111e40e3d7c7534d7'
