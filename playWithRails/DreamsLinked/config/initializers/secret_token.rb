e5f475aa813779e860da9babc195bbe1b9567ca4
Iruma Shibuya <programiruma@gmail.com>
2012-08-28 01:59:41 -0400

    Create DreamsLinked Rails App

---
+++ playWithRails/DreamsLinked/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamsLinked::Application.config.secret_token = 'f35dea0278f5942c5e24649254bbfea35b88c98d7d8667237edc058b4d6f90f5830f91302dfeccf7fc77c4c19c82f92ac2efba8064ffd77f14271126434e79d2'
