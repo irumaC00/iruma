eb8f49f04b3ce5e09a700955237399b4348b2e61
Iruma Shibuya <programiruma@gmail.com>
2012-09-06 02:44:14 -0400

    Removed DL Products

--- root/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_username = "dreams19_admin"; 
-$db_pass = "july17th2010"; 
-$db_name = "dreams19_dreamers";
-mysql_connect("$db_host","$db_username","$db_pass") or die (mysql_error());
-mysql_select_db("$db_name") or die ("no database");
-?>
